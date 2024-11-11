20ba2f520474af3814eefec112a9c86923b2be0b
Uzbek Imtiaz Cheema <101115968+Cheema24@users.noreply.github.com>
2024-11-11 14:27:56 +0000

    changes made to pattern, only names dob dod , parentF, parentM

--- data.py
+++ data.py
@@ -3,38 +3,28 @@ people_data = {
        "name": "Otto Emmersohn",
        "birth_date": "1974-06-05",
        "death_date": None,
-       "parents": [],
-       "children": ["Joy Emmersohn"],
-       "siblings": [],
-       "spouse": "Cornelia Emmersohn"
+       "parentF": [""],
+       "parentM": [""],
+
    },
    "Cornelia Emmersohn": {
        "name": "Cornelia Emmersohn",
        "birth_date": "1972-02-02",
        "death_date": None,
-       "parents": [],
-       "children": ["Joy Emmersohn"],
-       "siblings": [],
-       "spouse": "Otto Emmersohn"
+       "parentF": [""],
+       "parentM": [""],
+
+
    },
    "Joy Emmersohn": {
        "name": "Joy Emmersohn",
        "birth_date": "1995-03-03",
        "death_date": None,
-       "parents": ["Otto Emmersohn", "Cornelia Emmersohn"],
-       "children": [],
-       "siblings": [],
-       "spouse": None
+       "parentF": ["Cornelia Emmersohn"],
+       "parentM": ["Otto Emmersohn"],
+
    }
-   "Joy Emmersohn": {
-       "name": "Joy Emmersohn",
-       "birth_date": "1995-03-03",
-       "death_date": None,
-       "parents": ["Otto Emmersohn", "Cornelia Emmersohn"],
-       "children": [],
-       "siblings": [],
-       "spouse": None
-   }
+
 
 
 
